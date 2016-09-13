5a4ac9ee0962efd0116dc7bf2aa302b17ea06e1b
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-13 10:08:52 -0700

    Started project

---
+++ wp-google-analytics-api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WP Google Analytics API
+ *
+ * @package WP-Google-Analytics-API
+ */
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/* Check if class already exists. */
+if ( ! class_exists( 'GoogleAnalyticsAPI' ) ) {
+
+	/**
+	 * GoogleAnalyticsAPI class.
+	 */
+	class GoogleAnalyticsAPI {
+
+
+
+	}
+}
